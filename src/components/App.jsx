afe2a1cf362742b90d121f3221d0b70a94de44bc
Oleksii26 <Kolesnik26@ukr.net>
2022-12-11 22:42:11 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,22 @@
-import  ContactList  from "./Phonebook/ContactList";
+import ContactList from "./Phonebook/ContactList";
 import Filter from "./Filter/Filter";
-import { useState, useEffect, useMemo } from "react";
-import  PhoneBook  from "./Phonebook/Phonebook";
-
-const initContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]
+import { useMemo } from "react";
+import PhoneBook from "./Phonebook/Phonebook";
+import { useSelector } from "react-redux";
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ?? initContacts
-  })
+  const contacts = useSelector(state => state.contacts.contacts)
+  const filter = useSelector(state => state.contacts.filter)
 
-  const [filter, setFilter] = useState('')
-
-  const handleFormSubmit = profile => {
-    const nameToCheck = profile.name.toLocaleLowerCase()
-    const isIncludeName = contacts.some(contact => contact.name.toLocaleLowerCase() === nameToCheck)
-
-    if (isIncludeName) {
-      return alert(`${profile.name} is already in contacts`)
-    }
-    setContacts(prev => [profile, ...prev])
-  }
-
-  const handleFilterChange = e => {
-    const { value } = e.currentTarget
-    setFilter(value)
-  }
-
-  const getVisibleContacts = useMemo(() => {
+   const getVisibleContacts = useMemo(() => {
     const normalizedContacts = filter.toLocaleLowerCase()
     return contacts.filter(({ name }) => name.toLocaleLowerCase().includes(normalizedContacts))
   }, [contacts, filter])
 
-
-
-  const handleRemoveContact = id => {
-    const updateContacts = contacts.filter(contact => contact.id !== id)
-    setContacts(updateContacts)
-  }
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts))
-  }, [contacts])
-
   return <div>
-    <PhoneBook onFormSubmit={handleFormSubmit} />
-    <Filter value={filter} onChange={handleFilterChange} />
-    <ContactList contacts={getVisibleContacts} onRemove={handleRemoveContact} />
+    <PhoneBook />
+    <Filter />
+    <ContactList contacts={getVisibleContacts} />
   </div >
 
 }
--- src/components/Phonebook/ContactList.jsx
+++ src/components/Phonebook/ContactList.jsx
@@ -1,17 +1,12 @@
+import { ContactItems } from './ContactItems'
 import css from './PhoneBook.module.css'
 
-const ContactListItem = ({ id, name, number, onRemove }) => {
+const ContactList = ({contacts }) => {
 
-    return (<li key={id}  className={css.item}>
-        {name}: {number} <button  className={css.btnDelete} onClick={() => onRemove(id)}>delete</button>
-    </li>)
-}
-
-
-const ContactList = ({ contacts, onRemove }) => {
     if (contacts.length === 0) { return null }
+
     return (
-        <ul className={css.list}> {contacts.map(contact => <ContactListItem {...contact} onRemove={onRemove} />)}</ul>
+        <ul className={css.list}> {contacts.map(contact => <ContactItems key={contact.id} {...contact} />)}</ul>
     )
 
 }
