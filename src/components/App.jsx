29d585a49e47a0c4abdce23cf9e6215c82685513
Oleksii26 <Kolesnik26@ukr.net>
2022-12-13 08:48:37 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,13 @@
 import ContactList from "./Phonebook/ContactList";
 import Filter from "./Filter/Filter";
-import { useMemo } from "react";
 import PhoneBook from "./Phonebook/Phonebook";
-import { useSelector } from "react-redux";
 
 export const App = () => {
-  const contacts = useSelector(state => state.contacts.contacts)
-  const filter = useSelector(state => state.contacts.filter)
-
-   const getVisibleContacts = useMemo(() => {
-    const normalizedContacts = filter.toLocaleLowerCase()
-    return contacts.filter(({ name }) => name.toLocaleLowerCase().includes(normalizedContacts))
-  }, [contacts, filter])
 
   return <div>
-    <PhoneBook />
-    <Filter />
-    <ContactList contacts={getVisibleContacts} />
+    <PhoneBook/>
+    <Filter/>
+    <ContactList/>
   </div >
 
 }
--- src/components/Phonebook/ContactList.jsx
+++ src/components/Phonebook/ContactList.jsx
@@ -1,12 +1,22 @@
 import { ContactItems } from './ContactItems'
 import css from './PhoneBook.module.css'
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 
-const ContactList = ({contacts }) => {
+const ContactList = () => {
+
+    const contacts = useSelector(state => state.contacts.contacts)
+    const filter = useSelector(state => state.contacts.filter)
+
+    const getVisibleContacts = useMemo(() => {
+        const normalizedContacts = filter.toLocaleLowerCase()
+        return contacts.filter(({ name }) => name.toLocaleLowerCase().includes(normalizedContacts))
+    }, [contacts, filter])
 
     if (contacts.length === 0) { return null }
 
     return (
-        <ul className={css.list}> {contacts.map(contact => <ContactItems key={contact.id} {...contact} />)}</ul>
+        <ul className={css.list}> {getVisibleContacts.map(contact => <ContactItems key={contact.id} {...contact} />)}</ul>
     )
 
 }
